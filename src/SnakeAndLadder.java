bd68fbc6dca905ee1131a8472804f7b95453b962
ektashrikhande <ekta.shrikhande30@gmail.com>
2022-05-21 21:14:38 +0530

    first commit

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -1,8 +1,9 @@
 public class SnakeAndLadder {
-
     public static void main(String[] args) {
         System.out.println("Welcome to snake and ladder game");
-        int position = 0;
+        int position=0;
         System.out.println("Position of the player: "+ position);
+        int die = (int)(Math.random()*6) + 1;
+        System.out.println("Number on die: "+ die);
     }
 }
