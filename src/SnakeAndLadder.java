5970494b9f1387fd6faaaf440cf18a329431455a
ektashrikhande <ekta.shrikhande30@gmail.com>
2022-05-21 19:21:40 +0530

    UC_1 Added

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -1,2 +1,8 @@
 public class SnakeAndLadder {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to snake and ladder game");
+        int position = 0;
+        System.out.println("Position of the player: "+ position);
+    }
 }
