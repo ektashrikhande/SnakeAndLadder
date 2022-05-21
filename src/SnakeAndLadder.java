ca65e582671583731720561b69207a49360e0da4
ektashrikhande <ekta.shrikhande30@gmail.com>
2022-05-21 21:15:55 +0530

    Welcome to Snake And Ladder

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -3,7 +3,5 @@ public class SnakeAndLadder {
         System.out.println("Welcome to snake and ladder game");
         int position=0;
         System.out.println("Position of the player: "+ position);
-        int die = (int)(Math.random()*6) + 1;
-        System.out.println("Number on die: "+ die);
     }
 }
